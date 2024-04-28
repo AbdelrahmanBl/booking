459f2d44187fe149a269783737b2706477e6eee4
AbdelrahmanBl <abdelrahmangamal990@gmail.com>
2024-04-29 01:56:09 +0300

    [FEAT] prevent frontend guard for accessing invalid authed route

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -22,14 +22,29 @@ const router = createRouter({
 })
 
 router.beforeEach((to, from, next) => {
+    // handle none authed routes...
     if(to.meta.auth === undefined) {
         next()
     }
+    // handle authed user hitting none authed route...
     else if(AuthHelper.isAuthUser() && ! to.meta.auth) {
         AuthHelper.isAuthAdminOrEmployee()
         ? next({ name: 'admin.home' })
         : next({ name: 'client.home' })
     }
+    // handle authed user hitting invalid authed route...
+    else if(AuthHelper.isAuthUser() && to.meta.auth) {
+        if(to.name.search('admin.') > -1 && AuthHelper.isAuthClient()) {
+            next({ name: 'client.home' })
+        }
+        else if(to.name.search('client.') > -1 && AuthHelper.isAuthAdminOrEmployee()) {
+            next({ name: 'admin.home' })
+        }
+        else {
+            next()
+        }
+    }
+    // handle guest user hitting authed route...
     else if(AuthHelper.isGuestUser() && to.meta.auth) {
         to.name.search('admin.') > -1
         ? next({ name: 'admin.login' })
